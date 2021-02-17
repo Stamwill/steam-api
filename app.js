7febb98dfa98ed69dbdcfcb406fa3554e10e0b95
William <williamstam@gmail.com>
2021-02-17 17:53:54 +0000

    [app.js] Add cors and change listen to 3001

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const { graphqlHTTP } = require("express-graphql")
 const mongoose = require("mongoose")
 const graphqlSchema = require("./graphql/schema")
 const graphqlResolvers = require("./graphql/resolvers")
-
+const cors = require("cors")
 
 const app = express()
 
 app.use(
   "/graphql",
+  cors({
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
+  }),
   graphqlHTTP({
     schema: graphqlSchema,
     rootValue: graphqlResolvers,
@@ -19,7 +23,7 @@ const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWOR
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
 mongoose
   .connect(uri, options)
-  .then(() => app.listen(3000, console.log("server is running")))
+  .then(() => app.listen(3001, console.log("server is running")))
   .catch(error => {
     throw error
   })
